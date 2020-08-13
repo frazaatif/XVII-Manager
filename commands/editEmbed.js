9369e69aef275cba7d19ec2b6c86cf3195d94cc2
Aatif Fraz <imaatiffraz@gmail.com>
2020-08-13 22:16:03 +0530

    Update editEmbed.js

--- commands/editEmbed.js
+++ commands/editEmbed.js
@@ -6,52 +6,72 @@ module.exports = {
 	execute(allPeople, message) {
 		const Discord = require('discord.js');
 		
-		var emojis = ['⚔️','🪓','🏹','🐘','🛡️','🔭','🆎','🤷'];
+		var emojis = ['⚔️','🪓','🏹','🆎'];
 		
-		var people = {'⚔️':"",'🪓':"",'🏹':"",'🐘':"",'🛡️':"",'🔭':"",'🆎':"",'🤷':""};
+		var people = {'⚔️':[],'🪓':[],'🏹':[],'🆎':[]};
 		
 		const reactionManager = message.reactions.cache;
 		
 		const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 		
-		var startTime = new Date().getTime();
-		var endTime = new Date().getTime();
 
-		var time = 6000;
-		async function getReaction() {
-
-				reactionManager.each(async (messageReaction,emoji) => {
-				time -= 600;
-				await waitFor(time);
-
-				const useRs =  await message.reactions.resolve(emoji).users.fetch();
-				
-				for(const User of useRs){
-					
-					const user_id =  User[0];
-					if(user_id == '734080357040914582')return;
-					
-					const  name = allPeople[user_id];
-					
-					const nowPeople = people[emoji];
-					people[emoji] = nowPeople + name + "\n" ;
-					endTime = new Date().getTime();
-				}
-			
-			});
-		}
 		
 		
 		
 		
 		async function start() {
+	
+			var promises = reactionManager.map(async (MessageReaction, emoji) => {
+				if(!emojis.includes(emoji))return 
+				return MessageReaction.users.fetch()
+						.then((presUsers) => {
+							presUsers.forEach(async (Snowflake, User) => {
+								if(User != '734080357040914582') {
+									//var nowPeople = people[emoji];
+									//people[emoji] = nowPeople + ", " + allPeople[User];	
+									people[emoji].push(allPeople[User]);
+								}
+							});
+						}).catch((err) => console.log(err));
+			});
+			Promise.all(promises).then(() => {
+				console.log(people);
 
-			getReaction();
+				const descri = message.embeds[0].description;
+				const titl = message.embeds[0].title;
 			
-			await waitFor(10000);
+				const newMsg = new Discord.MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle(titl)
+					.setDescription(descri)
+					.addField("Zerg ⚔️","**Clash with enemy and push them back to their base**\n" + people['⚔️'].join(),false)
+					.addField("Breaker 🪓","**Dive into Enemy Artifact**\n" + people['🪓'].join(),false)
+					.addField("Support 🏹","**Use/Defend Cannons/Hwacha**\n" + people['🏹'].join(),false)		
+					.addField("Absent/Tentative 🆎","**Please Remember to state the reason!**\n" + people['🆎'].join(),true);
+
+				message.edit(newMsg);
+			});
+		}
+
+		start();
+	}
+};
+/*var time = 4000;
+			reactionManager.forEach(async (MessageReaction,emoji) => {
+				time -= 1000;
+				//await waitFor(time);
+				const presUsers = await MessageReaction.users.fetch();
+				//console.log(presUsers);
+				presUsers.forEach(async (Snowflake, User) => {
+					if(User != '734080357040914582') {
+						var nowPeople = people[emoji];
+						people[emoji] = nowPeople + ", " + allPeople[User];	
+					}
+				});
+			});
+			await waitFor(5000);
 			console.log(people);
 
-			console.log(endTime - startTime);
 			const descri = message.embeds[0].description;
 			const titl = message.embeds[0].title;
 			
@@ -59,20 +79,10 @@ module.exports = {
 			     .setColor('#0099ff')
  				 .setTitle(titl)
  				 .setDescription(descri)
- 				 .addField("Zerg ⚔️" , "𝗣𝘂𝘀𝗵 𝗘𝗻𝗲𝗺𝘆 𝗕𝗮𝗰𝗸 𝗧𝗼 𝗧𝗵𝗲𝗶𝗿 𝗕𝗮𝘀𝗲𝘀\n" + people['⚔️']    ,false)
- 				 .addField("Breaker 🪓" , "𝗗𝗶𝘃𝗲 𝗮𝗻𝗱 𝗕𝗿𝗲𝗮𝗸 𝗔𝗿𝘁𝗶𝗳𝗮𝗰𝘁\n" + people['🪓'],false)
- 				 .addField("Support 🛡️","𝗗𝗲𝗳𝗲𝗻𝗱 𝗛𝘄𝗮𝗰𝗵𝗮/𝗖𝗮𝗻𝗻𝗼𝗻𝘀\n" + people['🛡️'],false)
- 				 .addField("Cannoneer\\Hwacha 🏹" , "𝗨𝘀𝗲 𝗖𝗮𝗻𝗻𝗼𝗻𝘀/𝗛𝘄𝗮𝗰𝗵𝗮\n" + people['🏹'],true)
- 				 .addField("Mahout 🐘" , "𝗘𝗹𝗲𝗽𝗵𝗮𝗻𝘁 𝗥𝗶𝗱𝗲𝗿\n" + people['🐘'],true)
- 				 .addField("Scout 🔭 ","𝗙𝗶𝗻𝗱 𝗘𝗻𝗲𝗺𝘆 𝗕𝗮𝘀𝗲𝘀\n" + people['🔭'],true)		
-       	         .addField("Absent 🆎 ","zZzZ\n" + people['🆎'],true)
-       	         .addField("Tentative 🤷","????\n" + people['🤷'],true);
+ 				 .addField("Zerg ⚔️","**Clash with enemy and push them back to their base**\n" + people['⚔️'],false)
+				 .addField("Breaker 🪓","**Dive into Enemy Artifact**\n" + people['🪓'],false)
+				 .addField("Support 🏹","**Use/Defend Cannons/Hwacha**\n" + people['🏹'],false)		
+				 .addField("Absent/Tentative 🆎","**Please Remember to state the reason!**\n" + people['🆎'],true);
 
        	    message.edit(newMsg);
-
-		}
-
-		start();
-	}
-};
-/**/
+*/
