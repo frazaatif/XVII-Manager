b2424f6213d2af0d8b58bc8e8febf5e4cba24547
Aatif Fraz <imaatiffraz@gmail.com>
2020-08-25 21:46:56 +0530

    Update editEmbed.js

--- commands/editEmbed.js
+++ commands/editEmbed.js
@@ -10,9 +10,9 @@ module.exports = {
 		}
 		const Discord = require('discord.js');
 		
-		var emojis = ['⚔️','🪓','🏹','🆎'];
+		var emojis = [];
 		
-		var people = {'⚔️':[],'🪓':[],'🏹':[],'🆎':[]};
+		var people = {};
 		
 		const reactionManager = message.reactions.cache;
 		
@@ -26,14 +26,21 @@ module.exports = {
 		async function start() {
 			var totcount = 0,abscount = 0;
 			var promises = reactionManager.map(async (MessageReaction, emoji) => {
-				if(!emojis.includes(emoji))return 
+				if(!people.hasOwnProperty(emoji)){
+					people[emoji] = [];
+				}
 				return MessageReaction.users.fetch()
 						.then((presUsers) => {
 							var count = 0;
 							for(User of presUsers) {
 								if(User[0] == '734080357040914582')continue;
 								count++;
-								people[emoji].push("`" + count + ".`" + allPeople[User[0]]);
+								let pres_name = allPeople[User[0]];
+								let pres_emoji = emoji;
+								if(emoji== '736206573222887424'){pres_name += "<:cannon:736206573222887424>";pres_emoji = '🛡️';}
+								if(emoji == '🐘'){pres_name += "🐘";pres_emoji = '🛡️';}
+								if(emoji == '🔭'){pres_name += "🔭";pres_emoji = '🛡️';}
+								people[pres_emoji].push("`" + count + ".`" + pres_name);
 							}
 							if(emoji == '🆎')count = 0;
 							totcount += count;
@@ -54,7 +61,7 @@ module.exports = {
 					newMsg.addField("Available:`" + totcount + "`" ,"***Familiarize with your Roles and Join VC 10mins before for briefing!***",false)
 					.addField("Flex 🪓     ","**Dive Artifact**\n" + people['🪓'].join('\n'),true)
 					.addField("Zerg ⚔️   ","**Push Enemy Back**\n" + people['⚔️'].join('\n'),true)
-					.addField("Support 🏹 ","**Use/Defend\nCannons/Hwacha**\n" + people['🏹'].join('\n'),true)		
+					.addField("Support 🛡️","**Use/Defend Cannons,\n Elephant,Hwacha(🔭)\n**" + people['🛡️'].join('\n'),true)
 					.addField("Absent/Tentative 🆎","**Please Remember to state the reason!**\n" + people['🆎'].join(),false);
 				} else {
 					newMsg.addField("Available:`" + totcount + "`" ,"***Familiarize with your Roles and Join VC 10mins before for briefing!***",false)
@@ -71,33 +78,3 @@ module.exports = {
 		start();
 	}
 };
-/*var time = 4000;
-			reactionManager.forEach(async (MessageReaction,emoji) => {
-				time -= 1000;
-				//await waitFor(time);
-				const presUsers = await MessageReaction.users.fetch();
-				//console.log(presUsers);
-				presUsers.forEach(async (Snowflake, User) => {
-					if(User != '734080357040914582') {
-						var nowPeople = people[emoji];
-						people[emoji] = nowPeople + ", " + allPeople[User];	
-					}
-				});
-			});
-			await waitFor(5000);
-			console.log(people);
-			const descri = "Friendly War with AllegianceAE on Tuesday 20:30 (Server) at Kamasylve Valley"
-			//const descri = message.embeds[0].description;
-			const titl = message.embeds[0].title;
-			
-			const newMsg = new Discord.MessageEmbed()
-			     .setColor('#0099ff')
- 				 .setTitle(titl)
- 				 .setDescription(descri)
- 				 .addField("Zerg ⚔️","**Clash with enemy and push them back to their base**\n" + people['⚔️'],false)
-				 .addField("Breaker 🪓","**Dive into Enemy Artifact**\n" + people['🪓'],false)
-				 .addField("Support 🏹","**Use/Defend Cannons/Hwacha**\n" + people['🏹'],false)		
-				 .addField("Absent/Tentative 🆎","**Please Remember to state the reason!**\n" + people['🆎'],true);
-
-       	    message.edit(newMsg);
-*/
