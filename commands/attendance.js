c17d74ea44beb127df68928681aa5cd604a14aba
Aatif Fraz <imaatiffraz@gmail.com>
2020-08-25 21:48:11 +0530

    Update attendance.js

--- commands/attendance.js
+++ commands/attendance.js
@@ -7,6 +7,8 @@ module.exports = {
 	async execute (message, args) {
 		const Discord = require('discord.js');
 		
+		
+					
 		const askType = new Discord.MessageEmbed()
 							.setColor('#009900')
 							.setTitle('Type of required poll')
@@ -27,14 +29,27 @@ module.exports = {
 				descri = collected.last().content;
 				await dmchannel.send("Understood, The poll will be created shortly!");
 				if(opt == 'Node War') {
+					const cannon_emoji = message.client.emojis.cache.get('736206573222887424');
+					
 					finalmsg = new Discord.MessageEmbed()
 						.setColor('#0099ff')
 						.setTitle('Node War!')
 						.setDescription(`${descri}`)
 						.addField("Flex 🪓","**Dive Artifact**",true)
-						.addField("Zerg ⚔️","**Push Enemy Back**",true)
-						.addField("Support 🏹","**Use/Defend \n Cannons/Hwacha**",true)		
+						.addField(" Support 🛡️","**Use/Defend Cannons,Elephant,Hwacha(🔭)\n**",true)
+						.addField("Zerg ⚔️","**Push Enemy Back**",true)		
 						.addField("Absent/Tentative 🆎 ","**Please Remember to state the reason!**",false);
+					
+					await message.channel.send(finalmsg).then(async (sentMessage) => {
+						sentMessage.react('🪓')
+						.then(sentMessage.react('⚔️'))
+						.then(sentMessage.react('🛡️'))
+						.then(sentMessage.react(cannon_emoji))
+						.then(sentMessage.react('🐘'))
+						.then(sentMessage.react('🔭'))
+						.then(sentMessage.react('🆎'))
+						.catch((err) => console.log(err));
+					});
 				}else {
 					finalmsg = new Discord.MessageEmbed()
 						.setColor('#0099ff')
@@ -44,14 +59,16 @@ module.exports = {
 						.addField("Frontline 🪓       ","**Ball together & push**",true)
 						.addField("Backline 🏹         ","**Ranged DPS**",true)		
 						.addField("Absent/Tentative 🆎 ","**Please Remember to state the reason!**",false);
+				
+					await message.channel.send(finalmsg).then(async (sentMessage) => {
+						sentMessage.react('🪓')
+						.then(sentMessage.react('⚔️'))
+						.then(sentMessage.react('🏹'))
+						.then(sentMessage.react('🆎'))
+						.catch((err) => console.log(err));
+					});
 				}
-				await message.channel.send(finalmsg).then(async (sentMessage) => {
-					sentMessage.react('🪓')
-					.then(sentMessage.react('⚔️'))
-					.then(sentMessage.react('🏹'))
-					.then(sentMessage.react('🆎'))
-					.catch((err) => console.log(err));
-				});
+				
 					
 			}).catch(async (collected) => {
 				await channel.send("Ran into some problem, please try again!");
@@ -60,6 +77,6 @@ module.exports = {
 		}).catch(async (collected) => {
 			await channel.send("You either didnt reply within 2 minutes or gave an invalid option, try again!");
 		});
-
+		
 	}
  };
